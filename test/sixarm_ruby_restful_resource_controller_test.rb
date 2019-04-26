b6d65ab1e10361410eef97b23953e78f3ae905e6
Joel Parker Henderson <joel@joelparkerhenderson.com>
2019-04-25 20:21:31 -0700

    Update Minitest syntax

--- test/sixarm_ruby_restful_resource_controller_test.rb
+++ test/sixarm_ruby_restful_resource_controller_test.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require "sixarm_ruby_restful_resource_controller"
 
-class RestfulResourceControllerTest < Test::Unit::TestCase
+class RestfulResourceControllerTest < Minitest::Test
 
 end
 
